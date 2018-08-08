a6824e441b839a11060bfd3cb37c0749aec09671
Rosheeda <Rosheeda.Roberts@student.mec.cuny.edu>
2018-08-08 15:16:51 -0400

    Aug8

--- cssi-labs/python/labs/shopping-list-app/starter-code/shopping_list_starter.py
+++ cssi-labs/python/labs/shopping-list-app/starter-code/shopping_list_starter.py
@@ -15,19 +15,37 @@
 # limitations under the License.
 
 choice = ""
-
+#Level 1
 print("Welcome to the shopping list app!")
 
-shopping_list = []
+shopping_list = [blueberries, gojiberries, raspberries]
+    append[blueberries]
+    append[gojiberries]
+    append[raspberries]
 
 while choice.lower() != "e":
+    print ("                                          ")
     print("Please choose your action from the following list:")
     print("a. Add an item to the list")
     print("b. Remove an item from the list")
     print("c. Check to see if an item is on the list")
     print("d. Show all items on the list")
     print("e. exit")
-    
-    choice = input("Enter your choice [a|b|c|d|e]:")
-    
+    print ("                                       ")
+
+    choice =raw_input("Enter your choice [a|b|c|d|e]:"). lower ()
+    if choice =='a':
+        x = raw_input("Enter something to Add")
+        shopping_list.append(x)
+
+
+    #elif choice =='b':
+    #    pass
+    #elif choice =='c':
+    #    pass
+    #elif choice =='d':
+    #    pass
+
+
+
     # Your code below! Handle the cases when the user chooses a, b, c, d, or e
